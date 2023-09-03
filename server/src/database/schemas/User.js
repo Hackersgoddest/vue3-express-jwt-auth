8cfe3318cdec50776622bc0c60ec124b3be193c5
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-09-03 10:01:03 +0000

    [BE] added user schema using Sequelize

---
+++ server/src/database/schemas/User.js
@@ -0,0 +1,31 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    lowercase: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+});
+
+module.exports = User;
