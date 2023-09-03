390df70babc22d8b3f65c5337266de57b2f50572
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-09-03 09:55:23 +0000

    [BE] added database configuration using Sequelize

---
+++ server/src/database/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  logging: console.log,
+});
+
+module.exports = sequelize;
