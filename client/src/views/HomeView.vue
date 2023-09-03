d1caf81a5923e61d273a193f40787cf5cba7ed40
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-09-03 09:52:03 +0000

    [FE] added home component

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted} from "vue";
+import axios from "../axios";
+
+const user = ref({});
+async function getUserDetails() {
+  try {
+    const response = await axios.get("profile/");
+    user.value = response.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+onMounted(async () => {
+  getUserDetails();
+});
+</script>
+
+<template>
+  <main>
+    <h1>Welcome {{ user?.username || 'Comrade' }}</h1>
+  </main>
+</template>
