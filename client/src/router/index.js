43f6077884e828d90f11b703f7ff538ebc74a8ba
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-09-03 09:50:30 +0000

    [FE] added routes for login, register and and home pages

---
+++ client/src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import axios from "../axios";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/SignIn.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/SignUp.vue"),
+    },
+  ],
+});
+
+router.beforeEach(async (to, from) => {
+  try {
+    const authenticated = await is_authenticated();
+    if (to.meta.requiresAuth && !authenticated) {
+      // User is not authenticated, redirect to login
+      return { path: "/login" };
+    }
+    if ((to.path === "/login" || to.path === "/register") && authenticated) {
+      // User is authenticated and trying to access login, redirect to dashboard
+      return { path: "/" };
+    }
+  } catch (err) {
+    alert('server is down');
+  }
+});
+
+async function is_authenticated() {
+  try {
+    const response = await axios.get("profile/");
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+export default router;
