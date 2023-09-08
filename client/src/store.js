c8474593878640d8f5f4ccb4aef0152b314ed278
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-09-08 06:40:40 +0000

    [FE] added hasLogin and updateHasLogin to our simple store

--- client/src/store.js
+++ client/src/store.js
@@ -5,5 +5,9 @@ export const store = reactive({
   username: null,
   updateName(name) {
     this.username = name;
+  },
+  hasLogin: false,
+  updateHasLogin(value) {
+    this.hasLogin = value;
   }
 })
